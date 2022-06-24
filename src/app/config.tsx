1933fa7085b880754dfab8073c632b83989a9ad7
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-06-25 00:01:24 +0800

    feat: synology navigator

---
+++ src/app/config.tsx
@@ -0,0 +1,11 @@
+import Download from '/download.svg';
+import Folder from '/folder.svg';
+import Photos from '/photos.svg';
+import Video from '/video.svg';
+
+export const streetLightConfig = [
+  { src: Download, description: 'Download Station', to: '/download' },
+  { src: Folder, description: 'File Station', to: '/file' },
+  { src: Photos, description: 'Synology Photos', to: '/photo' },
+  { src: Video, description: 'Video Station', to: '/video' }
+];
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,32 @@
 import { FC } from 'react';
+import StreetLight from '@/components/StreetLight';
+import styles from './index.module.scss';
+import Avatar from '/avatar.webp';
+import { streetLightConfig } from './config';
 
 const App: FC = () => {
-  return <div>Hello World</div>;
+  const { VITE_PORT, VITE_ADDRESS } = import.meta.env;
+
+  return (
+    <div className={styles.wrapper}>
+      <a
+        className={styles.avatarWrapper}
+        href={`http://${VITE_ADDRESS}:${VITE_PORT}`}
+      >
+        <img
+          alt='avatar'
+          className={styles.avatar}
+          src={Avatar}
+          draggable={false}
+        />
+      </a>
+      <div className={styles.streetLightWrapper}>
+        {streetLightConfig.map((config, index) => (
+          <StreetLight key={index} {...config} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
