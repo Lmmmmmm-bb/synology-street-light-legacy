9587f7b624e90692f5c247ec47a94fdcf4032d90
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-06-24 13:33:10 +0800

    feat: init street light component

---
+++ src/components/StreetLight/index.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+import styles from './index.module.scss';
+
+interface IStreetLightProps {
+  to: string;
+  src: string;
+  description: string;
+}
+
+const StreetLight: FC<IStreetLightProps> = (props) => {
+  const { to, src, description } = props;
+
+  return (
+    <a
+      className={styles.wrapper}
+      href={`http://${import.meta.env.VITE_ADDRESS}${to}`}
+    >
+      <img
+        className={styles.image}
+        src={src}
+        alt={description}
+        draggable={false}
+      />
+      <p className={styles.description}>{description}</p>
+    </a>
+  );
+};
+
+export default StreetLight;
